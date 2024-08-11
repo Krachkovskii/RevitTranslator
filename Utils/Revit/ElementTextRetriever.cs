3d2193538d5c6cf9ef8a8ed9662f917a40857bfd
Krachkovskii <117347760+Krachkovskii@users.noreply.github.com>
2024-08-11 14:39:46 +0400

    Added name translation for schedules

--- Utils/Revit/ElementTextRetriever.cs
+++ Utils/Revit/ElementTextRetriever.cs
@@ -77,6 +77,7 @@ internal class ElementTextRetriever
     private void ProcessSchedule(ViewSchedule schedule)
     {
         ProcessScheduleHeaders(schedule);
+        ProcessElementName(schedule);
     }
 
     private void ProcessDimensionOverrides(Dimension dim)
