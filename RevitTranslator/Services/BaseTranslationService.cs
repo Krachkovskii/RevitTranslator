0bf0746d3d53326465ebbb81a1de75f0d827d7a3
Krachkovskii <117347760+Krachkovskii@users.noreply.github.com>
2024-12-31 18:00:23 +0400

    Move translation handling to separate services

--- RevitTranslator/Services/BaseTranslationService.cs
+++ RevitTranslator/Services/BaseTranslationService.cs
@@ -1,12 +1,60 @@
+using Autodesk.Revit.UI;
 using RevitTranslator.Contracts;
+using RevitTranslator.Models;
+using RevitTranslator.UI.Views;
+using RevitTranslator.Utils.App;
+using RevitTranslator.Utils.ElementTextRetrievers;
+using RevitTranslator.Utils.Revit;
+using RevitTranslator.ViewModels;
 
 namespace RevitTranslator.Services;
 
 public class BaseTranslationService : IService
 {
-    public List<ElementId> SelectedElements { get; set; } = [];
+    private ExternalEvent _externalEvent;
+    
+    public Element[] SelectedElements { get; set; } = [];
+    
     public void Execute()
     {
+        _externalEvent = ExternalEvent.Create(new ModelUpdateHandler());
+        var documentEntities = RetrieveText();
+
+        var viewModel = new ProgressWindowViewModel();
+        var view = new ProgressWindow(viewModel);
+        view.Show();
         
+        Translate(documentEntities);
+        UpdateRevitModel();
+    }
+
+    private List<DocumentTranslationEntityGroup> RetrieveText()
+    {
+        var retriever = new BatchTextRetriever();
+        return retriever.Translate(SelectedElements, false);
+    }
+    
+    private void Translate(List<DocumentTranslationEntityGroup> documentEntities)
+    {
+        var handler = new NewConcurrentTranslationHandler();
+        var cts = new CancellationTokenSource();
+
+        try
+        {
+            foreach (var documentEntity in documentEntities)
+            {
+                cts.Token.ThrowIfCancellationRequested();
+                Task.Run(async () => await handler.Execute(documentEntity.TranslationEntities, cts.Token), cts.Token);
+            }
+        }
+        catch (OperationCanceledException e)
+        {
+            // do nothing
+        }
+    }
+
+    private void UpdateRevitModel()
+    {
+        _externalEvent.Raise();
     }
 }
--- RevitTranslator/Services/CategoryTranslationService.cs
+++ RevitTranslator/Services/CategoryTranslationService.cs
@@ -8,6 +8,16 @@ public class CategoryTranslationService : IService
     public List<ObservableCategoryDescriptor> SelectedCategories { get; set; } = [];
     public void Execute()
     {
+        var elements = new FilteredElementCollector(Context.ActiveDocument)
+            .WherePasses(
+                new ElementMulticategoryFilter(SelectedCategories
+                    .Select(category => new ElementId(category.Id))
+                    .ToArray()))
+            .ToArray();
+
+        var service = new BaseTranslationService();
+        service.SelectedElements = elements;
         
+        service.Execute();
     }
 }
