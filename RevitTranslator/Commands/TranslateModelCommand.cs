5f03d48ecb27e7e97c4d35c459c29ca7b67f0921
Krachkovskii <117347760+Krachkovskii@users.noreply.github.com>
2025-01-01 19:47:12 +0400

    Code cleanup

--- RevitTranslator/Commands/TranslateModelCommand.cs
+++ RevitTranslator/Commands/TranslateModelCommand.cs
@@ -1,10 +1,8 @@
-﻿using System.Windows;
-using Nice3point.Revit.Toolkit.External;
+﻿using Nice3point.Revit.Toolkit.External;
 using RevitTranslator.Services;
 
 namespace RevitTranslator.Commands;
 
-//TODO: Move to separate service
 [UsedImplicitly]
 public class TranslateModelCommand : ExternalCommand
 {
@@ -15,25 +13,5 @@ public class TranslateModelCommand : ExternalCommand
         var service = new BaseTranslationService();
         service.SelectedElements = instances;
         service.Execute();
-    }
-
-    /// <summary>
-    /// Shows the warning that contains approximate number of elements. Allows to cancel the operation.
-    /// </summary>
-    /// <param name="count"></param>
-    /// <returns></returns>
-    private static bool ShowElementCountWarning(int count)
-    {
-        var result = MessageBox.Show($"You're about to translate all text properties of {count}+ elements. " +
-                $"It can be time-consuming and you might hit translation limits.\n\n" +
-                $"It will also translate elements such as view names or any annotations.\n" +
-                $"Consider selecting only necessary categories.\n\n" +
-                $"Are you sure you want to translate the whole model?",
-                "Large number of translations!",
-                MessageBoxButton.YesNo,
-                MessageBoxImage.Warning);
-
-        if (result == MessageBoxResult.Yes) { return true; }
-        else { return false; }
     }
 }
--- RevitTranslator/Services/CategoryTranslationService.cs
+++ RevitTranslator/Services/CategoryTranslationService.cs
@@ -6,6 +6,7 @@ namespace RevitTranslator.Services;
 public class CategoryTranslationService : IService
 {
     public List<ObservableCategoryDescriptor> SelectedCategories { get; set; } = [];
+    
     public void Execute()
     {
         var elements = new FilteredElementCollector(Context.ActiveDocument)
