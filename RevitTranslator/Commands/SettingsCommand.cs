c44943f84b2775f9a25c37146d66c67b74858ca0
Krachkovskii <117347760+Krachkovskii@users.noreply.github.com>
2025-01-08 10:30:07 +0400

    Add `TransactionMode` for commands

--- RevitTranslator/Commands/SettingsCommand.cs
+++ RevitTranslator/Commands/SettingsCommand.cs
@@ -1,10 +1,12 @@
-﻿using Nice3point.Revit.Toolkit.External;
+﻿using Autodesk.Revit.Attributes;
+using Nice3point.Revit.Toolkit.External;
 using RevitTranslator.UI.Views;
 using SettingsViewModel = RevitTranslator.ViewModels.SettingsViewModel;
 
 namespace RevitTranslator.Commands;
 
 [UsedImplicitly]
+[Transaction(TransactionMode.Manual)]
 public class SettingsCommand : ExternalCommand
 {
     public override void Execute()
--- RevitTranslator/Commands/TranslateSelectionCommand.cs
+++ RevitTranslator/Commands/TranslateSelectionCommand.cs
@@ -1,10 +1,12 @@
-﻿using Nice3point.Revit.Toolkit.External;
+﻿using Autodesk.Revit.Attributes;
+using Nice3point.Revit.Toolkit.External;
 using RevitTranslator.Extensions;
 using RevitTranslator.Services;
 
 namespace RevitTranslator.Commands;
 
 [UsedImplicitly]
+[Transaction(TransactionMode.Manual)]
 public class TranslateSelectionCommand : ExternalCommand
 {
     public override void Execute()
