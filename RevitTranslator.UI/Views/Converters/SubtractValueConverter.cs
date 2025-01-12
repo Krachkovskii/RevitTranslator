f0e5d0e1841128db4ceaa7fc396bd2d9c92bee56
Krachkovskii <117347760+Krachkovskii@users.noreply.github.com>
2025-01-12 13:35:58 +0400

    Fix converters

--- RevitTranslator.UI/Views/Converters/SubtractValueConverter.cs
+++ RevitTranslator.UI/Views/Converters/SubtractValueConverter.cs
@@ -6,16 +6,16 @@ namespace RevitTranslator.UI.Views.Converters;
 
 public class SubtractValueConverter : MarkupExtension, IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is not double doubleValue || parameter is not double) return null;
-        return doubleValue - (double)parameter;
+        if (value is not double doubleValue || parameter is not double doubleParameter) return null;
+        return doubleValue - doubleParameter;
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is not double doubleValue || parameter is not double) return null;
-        return doubleValue + (double)parameter;
+        if (value is not double doubleValue || parameter is not double doubleParameter) return null;
+        return doubleValue + doubleParameter;
     }
 
     public override object ProvideValue(IServiceProvider serviceProvider)
